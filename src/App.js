fb6389987eeb9a4ce4948f75a8f0853f338c63ac
Cuoi <tiennvde140153@fpt.edu.vn>
2022-09-17 17:15:56 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { actions, useStore } from './store';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useStore();
+    const { todos, todoInput } = state;
+
+    const handleAdd = () => {
+        dispatch(actions.addTodo(todoInput));
+    };
+
+    return (
+        <div>
+            <h2>Todo list</h2>
+            <input
+                value={todoInput}
+                placeholder="Enter Todo"
+                onChange={(e) => {
+                    dispatch(actions.setTodoInput(e.target.value));
+                }}
+            />
+            <button onClick={handleAdd}>Add</button>
+
+            {todos.map((todo, index) => (
+                <li key={index}>{todo}</li>
+            ))}
+        </div>
+    );
 }
 
 export default App;
