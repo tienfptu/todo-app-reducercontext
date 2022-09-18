e591a482869e91cabfad9d5bad884a8240476313
luc.le <trungluc.dev@gmail.com>
2022-09-18 10:00:41 +0700

    add filter

--- src/App.js
+++ src/App.js
@@ -3,75 +3,89 @@ import { actions, useStore } from './store';
 import './App.css';
 
 function App() {
-    const [state, dispatch] = useStore();
-    const { todos, todoInput } = state;
 
-    const handleAdd = () => {
-        dispatch(actions.addTodo(todoInput));
-    };
+  const [filter, setFilter] = useState('all')
 
-    const handleDelete = (id) => {
-        dispatch(actions.deleteTodo(id));
-    };
+  const [state, dispatch] = useStore();
+  const { todos, todoInput } = state;
 
-    const [search, setSearch] = useState('');
-    // const handleCheck = (id, checked) => {
-    //     dispatch(actions.checkTodo(id, checked));
-    // };
+  const handleAdd = () => {
+    dispatch(actions.addTodo(todoInput));
+  };
 
-    const handleCheck = (id) => {
-        dispatch(actions.checkTodo(id));
-    };
+  const handleDelete = (id) => {
+    dispatch(actions.deleteTodo(id));
+  };
 
-    return (
-        <div className="App">
-            <h2>Todo list</h2>
-            <div>
-                <input
-                    value={todoInput}
-                    placeholder="Enter Todo"
-                    onChange={(e) => {
-                        dispatch(actions.setTodoInput(e.target.value));
-                    }}
-                />
-                <button onClick={handleAdd}>Add</button>
-            </div>
-            <div>
-                <span>Search</span>
-                <input
-                    placeholder="Enter keywords "
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-            </div>
+  const [search, setSearch] = useState('');
+  // const handleCheck = (id, checked) => {
+  //     dispatch(actions.checkTodo(id, checked));
+  // };
 
-            {todos
-                .filter((e) => e.title.includes(search))
-                .map(({ id, title, status }) => (
-                    <li key={id}>
-                        {status ? (
-                            <strike>{title}</strike>
-                        ) : (
-                            <span>{title}</span>
-                        )}
+  const handleCheck = (id) => {
+    dispatch(actions.checkTodo(id));
+  };
 
-                        {/* <input
+  return (
+    <div className="App">
+      <h2>Todo list</h2>
+      <div>
+        <input
+          value={todoInput}
+          placeholder="Enter Todo"
+          onChange={(e) => {
+            dispatch(actions.setTodoInput(e.target.value));
+          }}
+        />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <div>
+        <span>Search</span>
+        <input
+          placeholder="Enter keywords "
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <button onClick={() => setFilter('all')}>All</button>
+        <button onClick={() => setFilter('complete')}>Complete</button>
+        <button onClick={() => setFilter('incomplete')}>Incomplete</button>
+      </div>
+
+      {todos
+        .filter((e) => e.title.includes(search))
+        .filter(e => {
+          if (filter === 'all') return true;
+          if (filter === 'complete') return e.status === true;
+          if (filter === 'incomplete') return e.status === false;
+        })
+        .map(({ id, title, status }) => (
+          <li key={id}>
+            {status ? (
+              <strike>{title}</strike>
+            ) : (
+              <span>{title}</span>
+            )}
+
+            {/* <input
                         type="checkbox"
                         onChange={(e) => {
                             handleCheck(id, e.currentTarget.checked);
                         }}
                     /> */}
 
-                        <input
-                            type="checkbox"
-                            checked={!!status}
-                            onChange={(e) => handleCheck(id)}
-                        />
+            <input
+              type="checkbox"
+              checked={!!status}
+              onChange={(e) => handleCheck(id)}
+            />
 
-                        <span onClick={() => handleDelete(id)}>&times;</span>
-                    </li>
-                ))}
-        </div>
-    );
+            <span onClick={() => handleDelete(id)}>&times;</span>
+          </li>
+        ))}
+    </div>
+  );
 }
 
 export default App;
